22f4319f3ffc1797b24afb1326a4dc455c42c3a0
Eddie P <eddiep@monark.com>
2021-02-10 15:23:53 -0800

    submit form to json & history forward after submit
    id gets added locally

--- dojo-blog/src/Create.js
+++ dojo-blog/src/Create.js
@@ -1,15 +1,40 @@
 import { useState } from "react";
+import { useHistory } from 'react-router-dom';
 
 const Create = () => {
 
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
     const [author, setAuthor] = useState('mario');
+    const [isLoading, setIsLoading] = useState(false);
+    const history = useHistory();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const blog = { title, body, author };
+
+        setIsLoading(true);
+        fetch('http://localhost:8000/blogs', {
+            method: 'POST',
+            headers: { "Content-Type": "application/json"},
+            body: JSON.stringify(blog)
+            //json server adds the id propery
+        }).then(() => {
+            console.log('new blog added');
+            setIsLoading(false);
+            // history.go(-1);
+            history.push('/');
+        })
+
+        
+        // console.log(blog);
+
+    }
 
     return ( 
         <div className="create">
             <h2>Add a New Blog Post</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>Blog title:</label>
                 <input 
                 type="text"
@@ -31,10 +56,11 @@ const Create = () => {
                     <option value="mario">mario</option>
                     <option value="yoshi">yoshi</option>
                 </select>
-                <button>Add Blog</button>
-                <p>{ title }</p>
-                <p>{ body }</p>
-                <p>{ author }</p>
+                {!isLoading && <button>Add Blog Post</button>}
+                {isLoading && <button disabled>Adding Blog Post</button>}
+                {/* <p>{ title }</p> */}
+                {/* <p>{ body }</p> */}
+                {/* <p>{ author }</p> */}
             </form>
         </div>
      );
