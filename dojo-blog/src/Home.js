62fd04808abee5ab67543cbf2eee1f777244475f
Eddie P <eddiep@monark.com>
2021-02-10 12:10:39 -0800

    testing buttons with events

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,7 +1,19 @@
 const Home = () => {
+    const handleClick = (e) => {
+        console.log('hello, ninjas', e);
+    }
+    const handleClickAgain = (name, e) => {
+        console.log('hello, ' + name, e.target);
+    }
     return ( 
         <div className="home">
             <h2>Home Page</h2>
+            <button onClick={handleClick}>Click Me</button>
+            <button onClick={(e) => {
+                handleClickAgain('mario', e)
+            }}>Click Again</button>
+            {/* if on one line, no need for extra curly braces */}
+            <button onClick={(e) => handleClickAgain('mario', e)}>Click Again</button>
         </div>
      );
 }
