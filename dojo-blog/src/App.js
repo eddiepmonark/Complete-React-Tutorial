8701952a0e19f491d74c08855d0659fb1aa71f55
Eddie P <eddiep@monark.com>
2021-02-10 11:50:29 -0800

    first commit

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
+
+  const title = 'Welcome to the new blog';
+  // const likes = 50;
+  // const person = { name: 'yoshi', age: 30 };
+  // const link = "https://google.com";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <div className="content">
+        {/* <h1>{ title }</h1> */}
+        <Home />
+        
+        {/* <p>Liked { likes } times</p>
+        <p>{ person }</p>
+        <p>{ "hello, ninjas "}</p>
+        <p>{ [1,2,3,4,5] }</p>
+        <p>{ Math.random() * 10 }</p>
+        <a href={link}>Google</a> */}
+      </div>
     </div>
   );
 }
