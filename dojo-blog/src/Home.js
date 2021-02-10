6f5faf42fadaba71883dfa9a8ce3977fcb42644f
Eddie P <eddiep@monark.com>
2021-02-10 12:29:33 -0800

    added blog with useState

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -2,21 +2,20 @@ import { useState } from 'react';
 
 
 const Home = () => {
-    // let name = 'mario';
-    const [name, setName] = useState('mario');
-    const [age, setAge] = useState(25);
-
-    const handleClick = () => {
-        // name = 'luigi';
-        // console.log(name);
-        setName('luigi');
-        setAge(30);
-    }
+    const [ blogs , setBlogs ] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ]);
     return ( 
         <div className="home">
-            <h2>Home Page</h2>
-            <button onClick={handleClick}>Click Me</button>
-            <p>{ name } is { age } years old</p>
+            {blogs.map((blog) => (
+                // always need to have key which is usually id
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{ blog.title }</h2>
+                    <p>By { blog.author }</p>
+                </div>
+            ))}            
         </div>
      );
 }
