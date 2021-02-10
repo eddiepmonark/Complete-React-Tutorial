8416700b47b2e98106f84d92d238363be2b6eb3f
Eddie P <eddiep@monark.com>
2021-02-10 12:51:43 -0800

    useEffect hook dependency

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 
@@ -8,16 +8,27 @@ const Home = () => {
         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ]);
+
+    const [name, setName] = useState('mario');
     
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
         setBlogs(newBlogs);
     }
 
+    useEffect(() => {
+        console.log('use effect ran');
+        // console.log(blogs);
+        console.log(name);
+        // use an empty dependency array [] to only run the function once on the first render
+    }, [name]);
+
     return ( 
         <div className="home">
             <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />  
             {/* <BlogList blogs={blogs.filter((blog) => blog.author === 'mario' )} title="Mario's Blogs!" />       */}
+            <button onClick={() => setName('luigi')}>change name</button>
+            <p>{ name }</p>
         </div>
      );
 }
